5ebd52de414692deded28235116c1aba05624e74
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-12-29 20:11:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,5 +48,12 @@ function createTimeInEvent(date) {
 }
 
 function hoursWorkedOnDate(date) {
-  let timeIn = this.timeInEvents.
+  let timeIn = this.timeInEvents.filter(d => d.date === date)[0]
+  let timeOut = this.timeOutEvents.filter(d => d.date === date)[0]
+
+  return (timeOut.hour - timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(date) {
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour 
 }
